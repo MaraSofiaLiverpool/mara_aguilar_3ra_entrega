1249af2b742ed571c0aa28f204da01f9e72234e9
MaraSofiaLiverpool <msaguilara@liverpool.com.mx>
2023-10-26 00:46:30 -0600

    Proyecto final

--- main.js
+++ main.js
@@ -1,4 +1,68 @@
+// Carrusel
+
+const carouselInner = document.getElementById("carouselInner")
+const carouselIndicators = document.getElementById("carouselIndicators")
+const traerDataCarousel = async () =>{
+    const response = await fetch("./dataCarousel.json")
+    const dataCarousel = await response.json()
+    dataCarousel.forEach((element,index) => {
+  
+        const div = document.createElement("div")
+        if( index == 0){
+            div.className= "carousel-item active"
+        }else{
+            div.className= "carousel-item"
+            const miBoton = document.createElement("button")
+            miBoton.setAttribute('type', 'button')
+            miBoton.setAttribute('data-bs-target', '#carouselExampleCaptions')
+            miBoton.setAttribute('data-bs-slide-to', index)
+            miBoton.setAttribute('aria-label', "Slide" + index)
+            carouselIndicators.append(miBoton)
+        }
+        
+        div.innerHTML = `<img src="${element.imagen}" class="d-block w-100" alt="...">
+                         <div class="carousel-caption d-none d-md-block">
+                            <h5>${element.titulo}</h5>
+                            <p>${element.descripcion}</p>
+                        </div>
+        `
+        carouselInner.append(div)
+    });
+}
+
+traerDataCarousel()
+
+
+// Calculadora
+
 const pacientes = JSON.parse( localStorage.getItem("pacientes")) || []
+
+const verPacientes = () => {
+    const contenedorPacientes = document.getElementById("contenedorPacientes");
+    contenedorPacientes.innerHTML = ""
+    
+    pacientes.forEach(paciente => {
+        const tarjetaPaciente = document.createElement("div");
+        tarjetaPaciente.className = "tarjetaPaciente";
+        tarjetaPaciente.innerHTML = `
+            <h3>${paciente.nombre} ${paciente.apellido}</h3>
+            <h4>${paciente.curp}</h4>
+            <button id="verConsultas${paciente.curp}">Ver consultas</button>
+            <div id="consultas${paciente.curp}" style="display: none;"></div>
+        `;
+    
+        contenedorPacientes.appendChild(tarjetaPaciente);
+    
+        const btnVerConsultas = document.getElementById(`verConsultas${paciente.curp}`);
+        const divConsultas = document.getElementById(`consultas${paciente.curp}`);
+
+       
+    
+        btnVerConsultas.addEventListener("click", () => {
+            mostrarConsultas(paciente, divConsultas);
+        });
+    });
+}
 
 const crearPaciente = (curp,nombre,apellido,direccion,tel) =>{
         const existe = pacientes.some(paciente => paciente.curp === curp)
@@ -19,18 +83,24 @@ const crearPaciente = (curp,nombre,apellido,direccion,tel) =>{
             }
             pacientes.push(paciente)
             localStorage.setItem("pacientes",JSON.stringify(pacientes))
+            verPacientes()
+
+            Swal.fire({
+                icon: 'succes',
+                title: 'Usuario creado exitosamente',
+             })
         }
 }
 
 const cargarPaciente = ()=>{
     const formCrearPaciente = document.getElementById("formCrearPaciente")
-formCrearPaciente.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    const datos = e.target.children
-    crearPaciente(datos["curp"].value,datos["nombre"].value,datos["apellido"].value,datos["direccion"].value,datos["tel"].value)
-    formCrearPaciente.reset()
+    formCrearPaciente.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        const datos = e.target.children
+        crearPaciente(datos["curp"].value,datos["nombre"].value,datos["apellido"].value,datos["direccion"].value,datos["tel"].value)
+        formCrearPaciente.reset()
 
-})
+    })
 
 }
 
@@ -89,7 +159,7 @@ const calcularImc = (curp,peso,altura) =>{
         localStorage.setItem("pacientes",JSON.stringify(pacientes))
         Swal.fire({
             icon: 'success',
-            title: 'Gracias por elegirnos',
+            title: 'Cálculo correcto',
              })
           
 }
@@ -104,31 +174,6 @@ const crearConsulta = () =>{
     })
 }
 
-const verPacientes = () => {
-    const contenedorPacientes = document.getElementById("contenedorPacientes");
-    
-    pacientes.forEach(paciente => {
-        const tarjetaPaciente = document.createElement("div");
-        tarjetaPaciente.className = "tarjetaPaciente";
-        tarjetaPaciente.innerHTML = `
-            <h3>${paciente.nombre} ${paciente.apellido}</h3>
-            <h4>${paciente.curp}</h4>
-            <button id="verConsultas${paciente.curp}">Ver consultas</button>
-            <div id="consultas${paciente.curp}" style="display: none;"></div>
-        `;
-    
-        contenedorPacientes.appendChild(tarjetaPaciente);
-    
-        const btnVerConsultas = document.getElementById(`verConsultas${paciente.curp}`);
-        const divConsultas = document.getElementById(`consultas${paciente.curp}`);
-
-       
-    
-        btnVerConsultas.addEventListener("click", () => {
-            mostrarConsultas(paciente, divConsultas);
-        });
-    });
-}
 
 const calcularUltimoIMC = (paciente) => {
     if (paciente.consultas.length > 0) {
@@ -165,6 +210,9 @@ const mostrarConsultas = (paciente, divConsultas) => {
 cargarPaciente()
 crearConsulta()
 verPacientes()
+
+
+
 
 
 
